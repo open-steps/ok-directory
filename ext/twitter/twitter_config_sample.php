86b1c2a705094f0003d0ddef81bcd9e71e049066
Open-Steps.org <contact@open-steps.org>
2014-07-23 11:11:36 +0200

    Added twitter_config_sample.php

---
+++ ext/twitter/twitter_config_sample.php
@@ -0,0 +1,11 @@
+<?php
+
+/** Set access tokens here - see: https://dev.twitter.com/apps/ **/
+$settings = array(
+    'oauth_access_token' => "yourdatahere",
+    'oauth_access_token_secret' => "yourdatahere",
+    'consumer_key' => "yourdatahere",
+    'consumer_secret' => "yourdatahere"
+);
+
+?>
