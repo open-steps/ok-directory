d68a7b4f01ff6e10b513b555d6b5112017be0fd2
Open-Steps.org <contact@open-steps.org>
2014-11-17 16:33:57 +0100

    Fixes #20 , implemented with https://github.com/acorbi/geocoder-cacher

--- config.example.js
+++ config.example.js
@@ -1,9 +1,9 @@
 window.plp = {
-	config: {
-		directory: "http://directory.plp.hackers4peace.net",
-		provider: "http://provider.plp.hackers4peace.net",
-		context: "http://plp.hackers4peace.net/context.jsonld",
-		mapQuestApiKey: "<API_KEY>",
-		twitterImageGetterUrl: "http://lifeformapps.com/lab/twitter-image-getter/twitter_profile_retriever.php"
-	}
+  config: {
+    directory: "http://directory.plp.hackers4peace.net",
+    provider: "http://provider.plp.hackers4peace.net",
+    context: "http://plp.hackers4peace.net/context.jsonld",
+    geocoderCacherUrl: "http://lifeformapps.com/lab/geocoder-cacher/geocoder_cacher.php",
+    twitterImageGetterUrl: "http://lifeformapps.com/lab/twitter-image-getter/twitter_profile_retriever.php"
+  }
 };
--- res/js/map.js
+++ res/js/map.js
@@ -1,19 +1,14 @@
-
 /*
  Geocoding
  ----------------------*/
-
 var map;
 var layerGroup;
 var markers;
 var fetchedAddresses = new Object();
-var mapQuestApiKey = window.plp.config.mapQuestApiKey; // Open Steps APIKey, change please
-var geocodeApiURL="http://www.mapquestapi.com/geocoding/v1/address?key="+mapQuestApiKey+"&country=#country#&city=#city#";
 var currentArticle = undefined;
 
 var initializeMap = function() {
 
-  // Init Map
   map = L.map('map').setView([13.4061, 52.5192], 1);
   L.tileLayer('http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg', {
       attribution: 'Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">',
@@ -21,26 +16,19 @@ var initializeMap = function() {
       }).addTo(map);
 
   markers = new L.MarkerClusterGroup({showCoverageOnHover: false});
-
   map.addLayer(markers);
-
-  // Disable wheel zoom
   map.scrollWheelZoom.disable();
 
 }
 
-// Calls the geocoding webservice for an article and adds the marker on the map when found. If it is the last one, set the map bounds so all markers are visible.
 var codeAddressFromArticle = function(context,article,last) {
 
   if (!article["about"]["address"][0]) return;
 
-  var addressToGeocode = article["about"]["address"][0]["country"]+', '+article["about"]["address"][0]["city"];
-
-  // Setup marker icon
+  var addressToGeocode = article["about"]["address"][0]["country"]+'_'+article["about"]["address"][0]["city"];
   var imgIcon = L.icon({
       iconUrl: 'res/img/dot.png',
       //shadowUrl: 'res/img/avatar_bg.png',
-
       iconSize:     [16, 16], // size of the icon
       //shadowSize:   [34, 34], // size of the shadow
       iconAnchor:   [8,8], // point of the icon which will correspond to marker's location
@@ -51,33 +39,27 @@ var codeAddressFromArticle = function(context,article,last) {
   // If the address was not already fetched, do it
   if (!fetchedAddresses[addressToGeocode]){
 
-    // replace placeholders in the url
-    var finalGeocodeApiURL = geocodeApiURL.replace('#city#',article["about"]["address"][0]["city"]);
-    finalGeocodeApiURL = finalGeocodeApiURL.replace('#country#',article["about"]["address"][0]["country"]);
+    var finalGeocodeApiURL =  window.plp.config.geocoderCacherUrl+"?location="+addressToGeocode;
+    superagent.get(finalGeocodeApiURL)
+    .withCredentials()
+    .set('Accept', 'text/plain')
+    .end(function(err,res){
 
-    //console.log("Fetching address on: "+finalGeocodeApiURL);
-
-    $.getJSON( finalGeocodeApiURL, function( data ) {
-
-      if (data.info.statuscode == 0 && data.results[0].locations[0]) {
-
-        // Store fetched address
-        fetchedAddresses[addressToGeocode] = data.results[0].locations[0].latLng.lat+","+data.results[0].locations[0].latLng.lng;
-
-        // Add marker
-        var marker = new L.marker(new L.latLng(data.results[0].locations[0].latLng.lat,data.results[0].locations[0].latLng.lng),{icon: imgIcon});
+      if (err){
+        console.log(err);
+      }else{
+        fetchedAddresses[addressToGeocode] = res.text;
+        var latLng = fetchedAddresses[addressToGeocode].split(",");
+        var marker = new L.marker(new L.latLng(latLng[0],latLng[1]),{icon: imgIcon});
         setupMarkerWithArticle(context,marker,article,last);
       }
 
     });
 
   }else{
-
-    // Extract coordinates from map
     var latLng = fetchedAddresses[addressToGeocode].split(",");
     var marker = new L.marker(new L.latLng(latLng[0],latLng[1]),{icon: imgIcon});
     setupMarkerWithArticle(context,marker,article,last);
-
   }
 
 }
@@ -88,59 +70,39 @@ var panMapToArticle = function(context,article){
   if (!article["about"]["address"][0]) return;
 
   var addressToGeocode = article["about"]["address"][0]["country"]+', '+article["about"]["address"][0]["city"];
-
-  // If the address was not already fetched, do it
   if (!fetchedAddresses[addressToGeocode]){
 
-    // replace placeholders in the url
-    var finalGeocodeApiURL = geocodeApiURL.replace('#city#',article["about"]["address"][0]["city"]);
-    finalGeocodeApiURL = finalGeocodeApiURL.replace('#country#',article["about"]["address"][0]["country"]);
+    var finalGeocodeApiURL =  window.plp.config.geocoderCacherUrl+"?location="+addressToGeocode;
+    superagent.get(finalGeocodeApiURL)
+    .withCredentials()
+    .set('Accept', 'text/plain')
+    .end(function(err,res){
 
-    //console.log("Fetching address on: "+finalGeocodeApiURL);
-
-    $.getJSON( finalGeocodeApiURL, function( data ) {
-
-      if (data.info.statuscode == 0 && data.results[0].locations[0]) {
-
-        // Store fetched address
-        fetchedAddresses[addressToGeocode] = data.results[0].locations[0].latLng.lat+","+data.results[0].locations[0].latLng.lng;
-
-        map.panTo(new L.latLng(data.results[0].locations[0].latLng.lat,data.results[0].locations[0].latLng.lng));
-        //map.setZoom(6);
+      if (err){
+        console.log(err);
+      }else{
+        fetchedAddresses[addressToGeocode] = res.text;
+        var latLng = fetchedAddresses[addressToGeocode].split(",");
+        map.panTo(new L.latLng(latLng[0],latLng[1]));
       }
 
     });
 
   }else{
-
-    // Extract coordinates from map
     var latLng = fetchedAddresses[addressToGeocode].split(",");
-
     map.panTo(new L.latLng(latLng[0],latLng[1]));
-    //map.setZoom(6);
-
   }
 
 }
 
-// Utility method to bind the popup of the marker and add it to the layer.
 var setupMarkerWithArticle = function(context,marker,article,last){
 
    marker.on('click', function (a) {
-
     currentArticle = article;
     nd._showArticleDetails(currentArticle);
-
-   });
-
-   marker.on('mouseout', function (a) {
-
-      //$('#details').html("<img src=\"res/img/avatar_placeholder.png\"/>");
-
    });
 
    markers.addLayer(marker);
-
    if (last){
      mapFitBounds();
    }
@@ -148,18 +110,9 @@ var setupMarkerWithArticle = function(context,marker,article,last){
 }
 
 var mapFitBounds = function(){
-
   map.fitBounds(markers.getBounds(),{padding: [50,50]});
-
 }
 
-/*--------------------
-  HELPERS
---------------------*/
-
 var clearLayers = function(){
-
-  //layerGroup.clearLayers();
   markers.clearLayers();
-
 };
